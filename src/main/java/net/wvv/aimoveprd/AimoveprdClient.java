e32657396f8fabc09fe96e5d03642398f23bfdb1
AndrewKorchemniy <andrewkorch@gmail.com>
2025-02-24 18:17:50 -0800

    Initial commit

---
+++ src/main/java/net/wvv/aimoveprd/AimoveprdClient.java
@@ -0,0 +1,10 @@
+package net.wvv.aimoveprd;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AimoveprdClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/wvv/aimoveprd/AimoveprdDataGenerator.java
@@ -0,0 +1,11 @@
+package net.wvv.aimoveprd;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AimoveprdDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
